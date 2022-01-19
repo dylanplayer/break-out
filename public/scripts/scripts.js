177d1452924b767d829f381958b53d7698303f73
Dylan Player <dylanplayer@Dylans-MacBook-Pro.local>
2022-01-19 15:05:09 -0800

    init

---
+++ public/scripts/scripts.js
@@ -0,0 +1,21 @@
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = '#FF0000';
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(240, 160, 20, 0, Math.PI*2);
+ctx.fillStyle = '#00FF00';
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.rect(160, 10, 100, 40);
+ctx.strokeStyle = '#0000FF';
+ctx.stroke();
+ctx.closePath();
+
