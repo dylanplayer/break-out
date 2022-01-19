1e9a38857b2cb14d757547555d40d6ee228a9aa5
Dylan Player <dylanplayer@Dylans-MacBook-Pro.local>
2022-01-19 15:31:52 -0800

    Finished Step 3

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -4,10 +4,13 @@ var ctx = canvas.getContext('2d');
 const WIDTH = canvas.width;
 const HEIGHT = canvas.height;
 
-let x = WIDTH / 2;
-let y = HEIGHT - 30
-let xSpeed = 2;
-let ySpeed = -2;
+// Ball Variables
+let ballX = WIDTH / 2;
+let ballY = HEIGHT - 30
+const ballRadius = 15;
+let ballXSpeed = 2;
+let ballYSpeed = -2;
+
 
 /**
  * Clears the canvas
@@ -16,20 +19,37 @@ const clearCanvas = () => {
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
 }
 
+/**
+ * Draws the ball on the canvas
+ */
 const drawBall = () => {
     ctx.beginPath();
-    ctx.arc(x, y, 15, 0, Math.PI * 2);
+    ctx.arc(ballX, ballY, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = '#0095DD';
     ctx.fill();
     ctx.closePath();
-    
 }
 
+/**
+ * All ball logic
+ */
+const ballLogic = () => {
+    if (ballX + ballRadius / 2 >= WIDTH - ballRadius / 2 || ballX - ballRadius / 2 <= 0 + ballRadius / 2){
+        ballXSpeed *= -1;
+    }
+    if (ballY + ballRadius / 2 >= HEIGHT - ballRadius / 2|| ballY - ballRadius / 2 <= 0 + ballRadius / 2){
+        ballYSpeed *= -1;
+    }
+    ballX += ballXSpeed;
+    ballY += ballYSpeed;
+}
+
+/**
+ * Runs every 10 Miliseconds
+ */
 const draw = () => {
     clearCanvas();
     drawBall();
-    x += xSpeed;
-    y += ySpeed;
-
+    ballLogic();
 }
 setInterval(draw, 10);
