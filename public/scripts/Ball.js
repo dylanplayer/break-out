d2fbc1f81d156914c8327eb2bde71033ce78def1
Dylan Player <dylanplayer@Dylans-MacBook-Pro.local>
2022-01-31 15:29:18 -0800

    Added Sprite

--- public/scripts/Ball.js
+++ public/scripts/Ball.js
@@ -1,17 +1,15 @@
+// eslint-disable-next-line import/extensions
+import Sprite from './Sprite.js';
+
 /* eslint-disable import/prefer-default-export */
-class Ball {
-  constructor(x, y, xSpeed, ySpeed) {
-    this.x = x;
-    this.y = y;
-    this.radius = 15;
+class Ball extends Sprite {
+  constructor(x, y, xSpeed, ySpeed, radius = 15, color = '#000000') {
+    super(x, y, 0, 0, color);
+    this.radius = radius;
     this.xSpeed = xSpeed;
     this.ySpeed = ySpeed;
-    this.color = '#000000';
   }
 
-  /**
-  * Draws the ball on the canvas
-  */
   draw(ctx) {
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
