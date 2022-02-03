a7337b0358c28dac34cdaae123fd08c63de12f82
Dylan Player <dylanplayer@Dylans-MacBook-Pro.local>
2022-02-02 20:23:58 -0800

    Added Game Class

---
+++ public/scripts/scripts2.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/extensions */
+import Game from './Game.js';
+
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
+
+const game = new Game(canvas, ctx);
+
+const draw = () => {
+  game.play();
+};
+
+draw();
